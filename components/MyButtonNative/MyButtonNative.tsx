bc4ac0ab913dde9f84144b4fc2269ce5d73a4231
MossPipat <pipat@magicsolution.com>
2024-03-18 16:00:36 +0700

    [update]:create button component

---
+++ components/MyButtonNative/MyButtonNative.tsx
@@ -0,0 +1,41 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+export default function MyButtonNative({ label }) {
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable
+        style={styles.button}
+        onPress={() => alert("You pressed a button.")}
+      >
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 3,
+    backgroundColor: "red",
+  },
+  button: {
+    borderRadius: 10,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  buttonIcon: {
+    paddingRight: 8,
+  },
+  buttonLabel: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
